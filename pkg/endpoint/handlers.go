e3757b3ff87064fdad18f2d38676c5a29afd9ccd
Arash Afshar <arash.afshar.edu@gmail.com>
2024-10-07 21:41:05 -0600

    use go echo

--- pkg/endpoint/handlers.go
+++ pkg/endpoint/handlers.go
@@ -1,57 +1,17 @@
 package endpoint
 
 import (
-	"html/template"
 	"log/slog"
 	"net/http"
-	"path/filepath"
 
-	dbPkg "github.com/Arash-Afshar/gohtmx/pkg/db"
+	"github.com/Arash-Afshar/gohtmx/pkg/db"
 	"github.com/Arash-Afshar/gohtmx/pkg/models"
-	"github.com/go-chi/chi/v5"
+	"github.com/labstack/echo/v4"
 )
 
-func ParsePartialTemplate(names ...string) *template.Template {
-	pagePath := filepath.Join(names...)
-	return template.Must(template.ParseFiles(pagePath))
-}
-
-func ParseTemplate(names ...string) *template.Template {
-	global := []string{
-		filepath.Join("templates", "main.html"),
-		filepath.Join("templates", "partials", "samples.html"), // TODO: glob
-	}
-
-	// Add all user templates after global.
-	pagePath := filepath.Join(names...)
-	global = append(global, pagePath)
-
-	return template.Must(template.ParseFiles(global...))
-}
-
-func ExecuteTemplate(w http.ResponseWriter, r *http.Request, data any, names ...string) bool {
-	tmpl := ParseTemplate(names...)
-
-	if err := tmpl.Execute(w, data); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		slog.Error(err.Error(), "method", r.Method, "status", http.StatusInternalServerError, "path", r.URL.Path)
-		return false
-	}
-	return true
-}
-
-func ExecutePartialTemplate(w http.ResponseWriter, r *http.Request, data any, names ...string) bool {
-	tmpl := ParsePartialTemplate(names...)
-
-	if err := tmpl.Execute(w, data); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		slog.Error(err.Error(), "method", r.Method, "status", http.StatusInternalServerError, "path", r.URL.Path)
-		return false
-	}
-	return true
-}
-
-func isHtmx(w http.ResponseWriter, r *http.Request) bool {
+func isHtmx(c echo.Context) bool {
+	w := c.Response()
+	r := c.Request()
 	// Check, if the current request has a 'HX-Request' header.
 	// For more information, see https://htmx.org/docs/#request-headers
 	if r.Header.Get("HX-Request") == "" || r.Header.Get("HX-Request") != "true" {
@@ -63,61 +23,48 @@ func isHtmx(w http.ResponseWriter, r *http.Request) bool {
 	return true
 }
 
-func (h *Handler) indexViewHandler(w http.ResponseWriter, r *http.Request) {
-	if !ExecuteTemplate(w, r, nil, "templates", "pages", "index.html") {
-		return
-	}
-	slog.Info("render page", "method", r.Method, "status", http.StatusOK, "path", r.URL.Path)
+func (h *Handler) indexViewHandler(c echo.Context) error {
+	return c.Render(http.StatusOK, "pages/index.html", nil)
 }
 
-func (h *Handler) apiListSampleHandler(w http.ResponseWriter, r *http.Request) {
-	if !isHtmx(w, r) {
-		return
-	}
-	samples, err := dbPkg.ListSamples(h.DB)
+func (h *Handler) apiListSampleHandler(c echo.Context) error {
+	samples, err := db.ListSamples(h.DB)
 	if err != nil {
-		slog.Error("Getting samples list", "errMessage", err, "method", r.Method, "status", http.StatusInternalServerError, "path", r.URL.Path)
-		http.Error(w, "Server error", http.StatusInternalServerError)
-		return
+		slog.Error("Getting samples list", "errMessage", err, "method", c.Request().Method, "status", http.StatusInternalServerError, "path", c.Request().URL.Path)
+		return c.Render(http.StatusInternalServerError, "pages/error.html", nil)
 	}
 	type data struct {
 		Samples []*models.Sample
 	}
-	if !ExecutePartialTemplate(w, r, data{Samples: samples}, "templates", "partials", "samples.html") {
-		return
-	}
-	slog.Info("render page", "method", r.Method, "status", http.StatusOK, "path", r.URL.Path)
+	return c.Render(http.StatusOK, "partials/samples.html", data{Samples: samples})
 }
 
-func (h *Handler) apiNewSampleHandler(w http.ResponseWriter, r *http.Request) {
-	if !isHtmx(w, r) {
-		return
+func (h *Handler) apiNewSampleHandler(c echo.Context) error {
+	if !isHtmx(c) {
+		return nil
 	}
-	name := r.FormValue("name")
+	name := c.FormValue("name")
 	sample := models.NewSample(name)
-	if err := dbPkg.AddSample(h.DB, sample); err != nil {
-		slog.Error("Add a sample", "errMessage", err, "method", r.Method, "status", http.StatusInternalServerError, "path", r.URL.Path)
-		http.Error(w, "Server error", http.StatusInternalServerError)
-		return
+	if err := db.AddSample(h.DB, sample); err != nil {
+		slog.Error("Add a sample", "errMessage", err, "method", c.Request().Method, "status", http.StatusInternalServerError, "path", c.Request().URL.Path)
+		return c.Render(http.StatusInternalServerError, "pages/error.html", nil)
 	}
-	h.apiListSampleHandler(w, r)
+	return h.apiListSampleHandler(c)
 }
 
-func (h *Handler) apiDeleteSampleHandler(w http.ResponseWriter, r *http.Request) {
-	if !isHtmx(w, r) {
-		return
+func (h *Handler) apiDeleteSampleHandler(c echo.Context) error {
+	if !isHtmx(c) {
+		return nil
 	}
-	name := chi.URLParam(r, "name")
-	sample, err := dbPkg.SampleByName(h.DB, name)
+	name := c.Param("name")
+	sample, err := db.SampleByName(h.DB, name)
 	if err != nil {
-		slog.Error("Find the sample", "errMessage", err, "method", r.Method, "status", http.StatusInternalServerError, "path", r.URL.Path)
-		http.Error(w, "Server error", http.StatusInternalServerError)
-		return
+		slog.Error("Find the sample", "errMessage", err, "method", c.Request().Method, "status", http.StatusInternalServerError, "path", c.Request().URL.Path)
+		return c.Render(http.StatusInternalServerError, "pages/error.html", nil)
 	}
-	if err := dbPkg.DeleteSample(h.DB, sample); err != nil {
-		slog.Error("Delete a sample", "errMessage", err, "method", r.Method, "status", http.StatusInternalServerError, "path", r.URL.Path)
-		http.Error(w, "Server error", http.StatusInternalServerError)
-		return
+	if err := db.DeleteSample(h.DB, sample); err != nil {
+		slog.Error("Delete a sample", "errMessage", err, "method", c.Request().Method, "status", http.StatusInternalServerError, "path", c.Request().URL.Path)
+		return c.Render(http.StatusInternalServerError, "pages/error.html", nil)
 	}
-	h.apiListSampleHandler(w, r)
+	return h.apiListSampleHandler(c)
 }
--- pkg/endpoint/server.go
+++ pkg/endpoint/server.go
@@ -2,15 +2,16 @@ package endpoint
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
-	"net/http"
-	"time"
+	"html/template"
+	"io"
+	"path/filepath"
 
-	dbPkg "github.com/Arash-Afshar/gohtmx/pkg/db"
-	"github.com/go-chi/chi/v5/middleware"
-
+	"github.com/Arash-Afshar/gohtmx/pkg/db"
 	"github.com/caarlos0/env/v10"
-	"github.com/go-chi/chi/v5"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Handler struct {
@@ -18,49 +19,87 @@ type Handler struct {
 }
 
 type Config struct {
-	Address      string `env:"GOHTMX_BACKEND_HOST" envDefault:"127.0.0.1:7000"`
-	ReadTimeout  int    `env:"GOHTMX_BACKEND_READ_TIMEOUT" envDefault:"5"`
-	WriteTimeout int    `env:"GOHTMX_BACKEND_WRITE_TIMEOUT" envDefault:"0"`
-	DbURL        string `env:"GOHTMX_BACKEND_DB_URL" envDefault:"sample.sqlite"`
+	Address string `env:"GOHTMX_BACKEND_HOST" envDefault:"127.0.0.1:7000"`
+	DbURL   string `env:"GOHTMX_BACKEND_DB_URL" envDefault:"sample.sqlite"`
 }
 
-// Run runs a new HTTP endpoint with the loaded environment variables.
+type Templates struct {
+	templates map[string]*template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	tmpl, ok := t.templates[name]
+	if !ok {
+		err := errors.New("Template not found -> " + name)
+		return err
+	}
+	return tmpl.Execute(w, data)
+}
+
+func NewTemplates() *Templates {
+	mainPath := "templates/main.html"
+	pagesGlob := "templates/pages/*.html"
+	partialsGlob := "templates/partials/*.html"
+
+	// find matched files
+	pages, err := filepath.Glob(pagesGlob)
+	if err != nil {
+		panic(err)
+	}
+	partials, err := filepath.Glob(partialsGlob)
+	if err != nil {
+		panic(err)
+	}
+
+	templates := make(map[string]*template.Template, 0)
+	for _, page := range pages {
+		files := []string{mainPath, page}
+		files = append(files, partials...)
+		t, err := template.ParseFiles(files...)
+		if err != nil {
+			panic(err)
+		}
+		templates["pages/"+filepath.Base(page)] = t
+	}
+
+	for _, partial := range partials {
+		t, err := template.ParseFiles(partial)
+		if err != nil {
+			panic(err)
+		}
+		templates["partials/"+filepath.Base(partial)] = t
+	}
+
+	return &Templates{
+		templates: templates,
+	}
+}
+
 func Run() error {
 	config := Config{}
 	if err := env.Parse(&config); err != nil {
 		panic(err)
 	}
 
-	// Create a new chi router.
-	router := chi.NewRouter()
-
-	// Use chi middlewares.
-	router.Use(middleware.Logger)
-
-	// Handle static files (with a custom handler).
-	router.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	router := echo.New()
+	router.Renderer = NewTemplates()
+	router.Use(middleware.Logger())
 
 	var err error
-	db, err := dbPkg.NewDB(config.DbURL)
+	dbInstance, err := db.NewDB(config.DbURL)
 	if err != nil {
 		return fmt.Errorf("db connection: %v", err)
 	}
 
-	h := Handler{DB: db}
+	h := Handler{DB: dbInstance}
 
 	// Handle pages
-	router.Get("/", h.indexViewHandler)
+	router.Static("/static", "static")
+	router.GET("/", h.indexViewHandler)
+	router.GET("/api/sample", h.apiListSampleHandler)
+	router.POST("/api/sample", h.apiNewSampleHandler)
+	router.DELETE("/api/sample/:name", h.apiDeleteSampleHandler)
 
-	// Handle apis
-	router.Get("/api/sample", h.apiListSampleHandler)
-	router.Post("/api/sample", h.apiNewSampleHandler)
-	router.Delete("/api/sample/{name}", h.apiDeleteSampleHandler)
-	server := &http.Server{
-		Addr:         config.Address,
-		Handler:      router, // handle all chi routes
-		ReadTimeout:  time.Duration(config.ReadTimeout) * time.Second,
-		WriteTimeout: time.Duration(config.WriteTimeout) * time.Second,
-	}
-
-	return server.ListenAndServe()
+	router.Logger.Fatal(router.Start(config.Address))
+	return nil
 }
